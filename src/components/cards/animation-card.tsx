6cf75b85c097946a261f6deb2786ea1c507baf42
qwanticode <75182974+qwanticode@users.noreply.github.com>
2023-12-12 12:03:07 +0100

    add link to entire card

--- src/components/cards/animation-card.tsx
+++ src/components/cards/animation-card.tsx
@@ -47,77 +47,81 @@ export function AnimationCard({
   const placeholderImage = "/images/placeholder-large-h.png";
 
   return (
-    <Card
-      className={cn(
-        "relative flex min-h-[500px] flex-col justify-between rounded-2xl border-none bg-transparent shadow-none",
-        className
-      )}
-      {...props}
-    >
-      <div
-        className={cn("transition-colors duration-200 ease-linear", bgColor)}
-      />
-      <CardHeader className="order-[-1] space-y-2">
-        <div className="flex h-44">
-          {props?.image?.data[0]?.attributes.url && (
-            <Image
-              src={props?.image?.data[0]?.attributes.url ?? placeholderImage}
-              alt={
-                props?.image?.data[0]?.attributes.alternativeText ??
-                "animation image"
-              }
-              width={300}
-              height={200}
-              className="h-full w-full rounded-md object-cover object-center"
-              sizes="(max-width: 768px) 50vw"
-              quality={75}
-              priority={false}
-            />
-          )}
-        </div>
-        <div>
-          <Title className="!m-0 h-32 min-h-[80px] w-full lg:!text-lg" level={3} isHTML>
-            {props.title}
-          </Title>
-          <Separator className=" bg-neutral-500" />
-        </div>
-      </CardHeader>
-      <CardContent className="flex min-h-[120px] flex-col justify-evenly space-y-2">
-        <div className="flex items-center text-[13px] leading-7 text-primary">
-          <IconContext.Provider value={{ className: "text-primary" }}>
-            <FaCalendarAlt className="mr-4" />
-          </IconContext.Provider>
-          {/* {formattedDate} */}
-          {/* <p className="text-cyan-500">
+    <Link href={`/programme-animations#${props.id}`}>
+      <Card
+        className={cn(
+          "relative flex min-h-[500px] flex-col justify-between rounded-2xl border-none bg-transparent shadow-none",
+          className
+        )}
+        {...props}
+      >
+        <div
+          className={cn("transition-colors duration-200 ease-linear", bgColor)}
+        />
+        <CardHeader className="order-[-1] space-y-2">
+          <div className="flex h-44">
+            {props?.image?.data[0]?.attributes.url && (
+              <Image
+                src={props?.image?.data[0]?.attributes.url ?? placeholderImage}
+                alt={
+                  props?.image?.data[0]?.attributes.alternativeText ??
+                  "animation image"
+                }
+                width={300}
+                height={200}
+                className="h-full w-full rounded-md object-cover object-center"
+                sizes="(max-width: 768px) 50vw"
+                quality={75}
+                priority={false}
+              />
+            )}
+          </div>
+          <div>
+            <Title
+              className="!m-0 h-32 min-h-[80px] w-full lg:!text-lg"
+              level={3}
+              isHTML
+            >
+              {props.title}
+            </Title>
+            <Separator className=" bg-neutral-500" />
+          </div>
+        </CardHeader>
+        <CardContent className="flex min-h-[120px] flex-col justify-evenly space-y-2">
+          <div className="flex items-center text-[13px] leading-7 text-primary">
+            <IconContext.Provider value={{ className: "text-primary" }}>
+              <FaCalendarAlt className="mr-4" />
+            </IconContext.Provider>
+            {/* {formattedDate} */}
+            {/* <p className="text-cyan-500">
               Ask date format because of multiline
             </p> */}
-          <span
-            className="text-xs font-semibold"
-            dangerouslySetInnerHTML={{
-              __html: props?.information?.date,
-            }}
-          />
-        </div>
-        <div className="flex items-center text-[13px] leading-7 text-primary">
-          <IconContext.Provider value={{ className: "text-primary" }}>
-            <FaMapMarkerAlt className="mr-4" />
-          </IconContext.Provider>
-          <span
-            className="text-xs font-semibold"
-            dangerouslySetInnerHTML={{
-              __html: cleanAddress,
-            }}
-          />
-        </div>
-      </CardContent>
-      <CardFooter className="">
-        <Link href={`/programme-animations#${props.id}`}>
+            <span
+              className="text-xs font-semibold"
+              dangerouslySetInnerHTML={{
+                __html: props?.information?.date,
+              }}
+            />
+          </div>
+          <div className="flex items-center text-[13px] leading-7 text-primary">
+            <IconContext.Provider value={{ className: "text-primary" }}>
+              <FaMapMarkerAlt className="mr-4" />
+            </IconContext.Provider>
+            <span
+              className="text-xs font-semibold"
+              dangerouslySetInnerHTML={{
+                __html: cleanAddress,
+              }}
+            />
+          </div>
+        </CardContent>
+        <CardFooter className="">
           <Button variant="ghost" className="p-0">
             Plus de détails
             <ButtonIcon variant="default" />
           </Button>
-        </Link>
-      </CardFooter>
-    </Card>
+        </CardFooter>
+      </Card>
+    </Link>
   );
 }
--- src/components/cards/exhibitor-card.tsx
+++ src/components/cards/exhibitor-card.tsx
@@ -64,151 +64,149 @@ export function ExhibitorCard({
   const placeholderImage = "/images/placeholder-large-h.png";
 
   return (
-    <>
-      <Card
-        className={cn(
-          "flex h-96 flex-col justify-between rounded-2xl border-none shadow-none",
-          isSelected ? "!bg-accent" : "",
-          className
-        )}
-        {...props}
-        onMouseEnter={() => setIsSelected(true)}
-        onMouseLeave={() => setIsSelected(false)}
-      >
-        <CardHeader>
-          {tagValue && (
-            <div
-              className={`mb-auto flex justify-end justify-items-center ${
-                isSelected ? "text-gray-200" : "text-primary"
-              }`}
-            >
-              <span className=" hover:main-color pr-2.5 text-base font-normal">
-                {tagTitle ?? "Stand"}
-              </span>
-              <span
-                className={`inline-block rounded-xl border px-1.5 py-0.5 text-sm font-semibold uppercase ${
-                  isSelected ? "border-gray-200" : "border-primary"
+    <Dialog>
+      <DialogTrigger className="text-left rounded-2xl">
+        <Card
+          className={cn(
+            "flex h-96 flex-col justify-between rounded-2xl border-none shadow-none",
+            isSelected ? "!bg-accent" : "",
+            className
+          )}
+          {...props}
+          onMouseEnter={() => setIsSelected(true)}
+          onMouseLeave={() => setIsSelected(false)}
+        >
+          <CardHeader>
+            {tagValue && (
+              <div
+                className={`mb-auto flex justify-end justify-items-center ${
+                  isSelected ? "text-gray-200" : "text-primary"
                 }`}
               >
+                <span className=" hover:main-color pr-2.5 text-base font-normal">
+                  {tagTitle ?? "Stand"}
+                </span>
+                <span
+                  className={`inline-block rounded-xl border px-1.5 py-0.5 text-sm font-semibold uppercase ${
+                    isSelected ? "border-gray-200" : "border-primary"
+                  }`}
+                >
+                  {tagValue ?? "A00"}
+                </span>
+              </div>
+            )}
+            <div className="flex h-32">
+              {props?.plan?.data?.attributes.url && (
+                <Image
+                  src={props?.plan?.data?.attributes.url ?? placeholderImage}
+                  alt={
+                    props?.plan?.data?.attributes.alternativeText ??
+                    "animation image"
+                  }
+                  width={300}
+                  height={200}
+                  className="h-full w-full rounded-md object-cover object-center"
+                  sizes="(max-width: 768px) 50vw"
+                  quality={75}
+                  priority={false}
+                />
+              )}
+            </div>
+          </CardHeader>
+          <CardContent className="py-0">
+            <h2
+              className={`mb-4 font-title text-xl font-semibold uppercase ${
+                isSelected ? "text-gray-100" : ""
+              }`}
+            >
+              {props?.title}
+            </h2>
+            <p
+              className={`text-base font-normal ${
+                isSelected ? "text-gray-300" : ""
+              }`}
+            >
+              {truncatedDescription}
+            </p>
+          </CardContent>
+          <CardFooter>
+            {isSelected && (
+              <Button
+                isDialog
+                variant="ghost"
+                className="mt-4 p-0 text-secondary"
+              >
+                {buttonLabel}
+                <ButtonIcon variant="accent" />
+              </Button>
+            )}
+          </CardFooter>
+        </Card>
+      </DialogTrigger>
+      <DialogContent>
+        <ScrollArea className="h-[80vh] w-full rounded-lg md:p-10">
+          <DialogHeader>
+            <div className="flex justify-items-center pb-2 pr-12 text-primary">
+              <span className="pr-2.5 text-base font-normal">
+                {tagTitle ?? "Stand"}
+              </span>
+              <span className="inline-block rounded-xl border border-primary px-1.5 py-0.5 text-sm font-semibold uppercase">
                 {tagValue ?? "A00"}
               </span>
             </div>
-          )}
-          <div className="flex h-32">
-            {props?.plan?.data?.attributes.url && (
-              <Image
-                src={props?.plan?.data?.attributes.url ?? placeholderImage}
-                alt={
-                  props?.plan?.data?.attributes.alternativeText ??
-                  "animation image"
-                }
-                width={300}
-                height={200}
-                className="h-full w-full rounded-md object-cover object-center"
-                sizes="(max-width: 768px) 50vw"
-                quality={75}
-                priority={false}
-              />
-            )}
+            <Title
+              className="main-color mb0 max-w-xl pr-12 font-semibold"
+              level={2}
+              isHTML
+            >
+              {props?.title ?? "Titre"}
+            </Title>
+          </DialogHeader>
+          <div className="my-5 inline-block rounded-lg bg-card px-4 py-2 text-base font-medium text-primary">
+            {props.category.data?.attributes.name}
           </div>
-        </CardHeader>
-        <CardContent className="py-0">
-          <h2
-            className={`mb-4 font-title text-xl font-semibold uppercase ${
-              isSelected ? "text-gray-100" : ""
-            }`}
-          >
-            {props?.title}
-          </h2>
-          <p
-            className={`text-base font-normal ${
-              isSelected ? "text-gray-300" : ""
-            }`}
-          >
-            {truncatedDescription}
-          </p>
-        </CardContent>
-        <CardFooter>
-          {isSelected && (
-            <Dialog>
-              <DialogTrigger className="inline-flex items-center">
-                <Button
-                  isDialog
-                  variant="ghost"
-                  className="mt-4 p-0 text-secondary"
-                >
-                  {buttonLabel}
-                  <ButtonIcon variant="accent" />
-                </Button>
-              </DialogTrigger>
-              <DialogContent>
-                <ScrollArea className="h-[80vh] w-full rounded-lg md:p-10">
-                  <DialogHeader>
-                    <div className="flex justify-items-center pb-2 pr-12 text-primary">
-                      <span className="pr-2.5 text-base font-normal">
-                        {tagTitle ?? "Stand"}
-                      </span>
-                      <span className="inline-block rounded-xl border border-primary px-1.5 py-0.5 text-sm font-semibold uppercase">
-                        {tagValue ?? "A00"}
-                      </span>
-                    </div>
-                    <Title
-                      className="main-color mb0 max-w-xl pr-12 font-semibold"
-                      level={2}
-                      isHTML
-                    >
-                      {props?.title ?? "Titre"}
-                    </Title>
-                  </DialogHeader>
-                  <div className="my-5 inline-block rounded-lg bg-card px-4 py-2 text-base font-medium text-primary">
-                    {props.category.data?.attributes.name}
+          <DialogDescription className="pb-5 text-base font-normal">
+            {props?.description}
+          </DialogDescription>
+          {props?.plan?.data?.attributes.url && (
+            <DialogFooter>
+              <div className="relative w-full">
+                {isFullscreen ? (
+                  <div
+                    className="fixed left-[50%] top-[50%] z-50 m-auto flex h-screen w-screen translate-x-[-50%] translate-y-[-50%] items-center justify-center bg-black bg-opacity-80"
+                    onClick={() => setIsFullscreen(false)}
+                  >
+                    <Image
+                      src={props?.plan?.data?.attributes.url}
+                      alt={
+                        props?.plan?.data?.attributes.alternativeText ??
+                        "exposant popup image"
+                      }
+                      layout="fill"
+                      objectFit="contain"
+                      className="cursor-zoom-out rounded-md"
+                      quality={100}
+                    />
                   </div>
-                  <DialogDescription className="pb-5 text-base font-normal">
-                    {props?.description}
-                  </DialogDescription>
-                  {props?.plan?.data?.attributes.url && (
-                    <DialogFooter>
-                      <div className="relative w-full">
-                        {isFullscreen ? (
-                          <div
-                            className="fixed left-[50%] top-[50%] z-50 m-auto flex h-screen w-screen translate-x-[-50%] translate-y-[-50%] items-center justify-center bg-black bg-opacity-80"
-                            onClick={() => setIsFullscreen(false)}
-                          >
-                            <Image
-                              src={props?.plan?.data?.attributes.url}
-                              alt={
-                                props?.plan?.data?.attributes.alternativeText ??
-                                "exposant popup image"
-                              }
-                              layout="fill"
-                              objectFit="contain"
-                              className="cursor-zoom-out rounded-md"
-                              quality={100}
-                            />
-                          </div>
-                        ) : (
-                          <Image
-                            src={props?.plan?.data?.attributes.url}
-                            alt={
-                              props?.plan?.data?.attributes.alternativeText ??
-                              "exposant popup image"
-                            }
-                            width={1400}
-                            height={900}
-                            className="cursor-zoom-in rounded-md object-cover"
-                            quality={100}
-                            onClick={handleImageClick}
-                          />
-                        )}
-                      </div>
-                    </DialogFooter>
-                  )}
-                </ScrollArea>
-              </DialogContent>
-            </Dialog>
+                ) : (
+                  <Image
+                    src={props?.plan?.data?.attributes.url}
+                    alt={
+                      props?.plan?.data?.attributes.alternativeText ??
+                      "exposant popup image"
+                    }
+                    width={1400}
+                    height={900}
+                    className="cursor-zoom-in rounded-md object-cover"
+                    quality={100}
+                    onClick={handleImageClick}
+                  />
+                )}
+              </div>
+            </DialogFooter>
           )}
-        </CardFooter>
-      </Card>
-    </>
+        </ScrollArea>
+      </DialogContent>
+    </Dialog>
   );
 }

bd1882f17093a516c95e4bc022aa4b3bc39daad2
qwanticode <75182974+qwanticode@users.noreply.github.com>
2023-12-17 15:15:02 +0100

    informations displayed as col

--- src/app/informations-pratiques/information-section.tsx
+++ src/app/informations-pratiques/information-section.tsx
@@ -5,37 +5,38 @@ export default function InformationSection({ domain, mainSection }) {
   return (
     <div className="flex flex-wrap pb-16 pt-5 lg:pb-20 xl:pb-24">
       <div className="w-full space-y-6 lg:w-1/2 lg:pr-6">
-        <div className="inline-flex">
-          <div className="icon-with-circle h-min">
-            <IoCalendarOutline />
-          </div>
-          <div className="w-[calc(100%-32px)] pl-4">
-            <p className="flex pb-4 text-2xl font-bold text-primary">
-              {mainSection.schedules.title}
-            </p>
-            <div
-              className="text-base"
-              dangerouslySetInnerHTML={{
-                __html: mainSection.schedules.description,
-              }}
-            ></div>
-          </div>
-        </div>
-
-        <div className="inline-flex">
-          <div className="icon-with-circle h-min">
-            <IoTicketOutline />
-          </div>
-          <div className="w-[calc(100%-32px)] pl-4">
-            <div className="font-semibold text-primary">
-              {mainSection.price.title}
+        <div className="flex flex-col space-y-4">
+          <div className="inline-flex">
+            <div className="icon-with-circle h-min">
+              <IoCalendarOutline />
             </div>
-            <div
-              className="text-base"
-              dangerouslySetInnerHTML={{
-                __html: mainSection.price.description,
-              }}
-            ></div>
+            <div className="w-[calc(100%-32px)] pl-4">
+              <p className="flex pb-4 text-2xl font-bold text-primary">
+                {mainSection.schedules.title}
+              </p>
+              <div
+                className="text-base"
+                dangerouslySetInnerHTML={{
+                  __html: mainSection.schedules.description,
+                }}
+              ></div>
+            </div>
+          </div>
+          <div className="inline-flex">
+            <div className="icon-with-circle h-min">
+              <IoTicketOutline />
+            </div>
+            <div className="w-[calc(100%-32px)] pl-4">
+              <div className="font-semibold text-primary">
+                {mainSection.price.title}
+              </div>
+              <div
+                className="text-base"
+                dangerouslySetInnerHTML={{
+                  __html: mainSection.price.description,
+                }}
+              ></div>
+            </div>
           </div>
         </div>
       </div>

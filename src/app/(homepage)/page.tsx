cbd1d2a3f507be4c8315548b8ea41e0dd4210e94
qwanticode <75182974+qwanticode@users.noreply.github.com>
2023-12-17 14:59:01 +0100

    randomize exhibitors display

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -48,7 +48,18 @@ export default async function HomePage() {
     "subsiteExhibitorContents"
   );
 
-  const limitedExhibitorsContentData = exhibitorsContentData.slice(0, 15);
+
+  //Fisher-Yates algorithm to shuffle the array
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+  const shuffledExhibitorsContentData = shuffleArray(exhibitorsContentData);
+  const limitedExhibitorsContentData = shuffledExhibitorsContentData.slice(0, 15);
 
   const animationsData = animationsPagesData.map((d: any) => d.sections).flat();
 

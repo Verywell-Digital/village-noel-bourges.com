ed1fa7055962f0dd99407245f47624c193d38357
qwanticode <75182974+qwanticode@users.noreply.github.com>
2024-02-01 00:50:13 +0100

    cleaning page

--- src/app/(visitor)/plan-du-salon/page.tsx
+++ src/app/(visitor)/plan-du-salon/page.tsx
@@ -1,12 +1,8 @@
 import Heading from "@/components/sections/heading"; // Import a shared heading component
 import { GET_LAYOUT, GET_SALON_MAP_PAGE } from "@/lib/gql";
 import { getGqlData } from "@/utils/get-graphql-data";
-import { Button } from "@/components/ui/button";
-import ButtonIcon from "@/components/ui/button-icon";
 import { Metadata } from "next";
 import PDFViewer from "@/components/media/pdf-viewer";
-
-// export const revalidate = 60;
 
 export async function generateMetadata(): Promise<Metadata> {
   const data = await getGqlData(GET_SALON_MAP_PAGE, "subsiteSalonMapPages");
@@ -21,9 +17,6 @@ async function Page() {
   const data = await getGqlData(GET_SALON_MAP_PAGE, "subsiteSalonMapPages");
   const salonMapPageData = data[0];
 
-  const dataLayout = await getGqlData(GET_LAYOUT, "subsiteLayouts");
-  const seo = dataLayout[0].seo;
-
   return (
     salonMapPageData && (
       <div className="container flex flex-col space-y-20 pb-32">
@@ -34,23 +27,6 @@ async function Page() {
               isHtml
               title={salonMapPageData?.title}
             />
-            {/* {salonMapPageData.pdf?.data.attributes.url &&
-            !salonMapPageData.pdf?.data.attributes.url.includes("dummy") && (
-              <a
-                href={salonMapPageData.pdf?.data.attributes.url}
-                download
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Button
-                  className="flex h-14 w-fit min-w-max flex-row justify-between text-sm"
-                  size="lg"
-                >
-                  {salonMapPageData.download.label}
-                  <ButtonIcon variant="secondary" />
-                </Button>
-              </a>
-            )} */}
           </div>
         </section>
         <section className="rounded-2xl bg-card lg:p-10">
@@ -58,11 +34,7 @@ async function Page() {
           !salonMapPageData?.pdf?.data?.attributes.url.includes("dummy") ? (
             <PDFViewer file={salonMapPageData?.pdf?.data?.attributes} />
           ) : (
-            <span dangerouslySetInnerHTML={{ __html: salonMapPageData?.notAvailableMessage || "" }} className="mx-auto flex w-full max-w-4xl p-5 font-title text-xl font-bold text-accent lg:text-xl">
-              {/* Le plan de l&lsquo;édition {new Date().getFullYear()} du{" "}
-              {seo?.meta_Title} n&lsquo;est pas encore disponible, merci de
-              consulter cette page ultérieurement. */}
-            </span>
+            <span dangerouslySetInnerHTML={{ __html: salonMapPageData?.notAvailableMessage || "" }} className="mx-auto flex w-full max-w-4xl p-5 font-title text-xl font-bold text-accent lg:text-xl" />
           )}
         </section>
       </div>
